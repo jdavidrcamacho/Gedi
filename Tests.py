96d6c83213a73174ec26bff402d3766115fb5a06
jdavidrcamacho <jdavidrcamacho@gmail.com>
2017-03-13 14:38:50 +0000

    tests with examples of everything developed

--- Tests.py
+++ Tests.py
@@ -8,21 +8,17 @@ import Kernel;reload(Kernel);kl=Kernel
 import Kernel_likelihood;reload(Kernel_likelihood);lk=Kernel_likelihood
 import Kernel_optimization;reload(Kernel_optimization);opt=Kernel_optimization
 
-import george
-import george.kernels as ge
-
 import numpy as np
+import matplotlib.pylab as pl
 
 #####  DADOS INICIAS  #########################################################
 np.random.seed(12345)
 x = 10 * np.sort(np.random.rand(101))
 yerr = 0.2 * np.ones_like(x)
 y = np.sin(x) + yerr * np.random.randn(len(x))
+
 ###############################################################################
 #TESTS FOR THE LIKELIHOOD AND GRADIENT
-
-print '########## Just tests ##############'
-
 print 'test 1'
 kernel1=kl.ExpSquared(11.0,7.0)
 kernel1_test1= lk.likelihood(kernel1,x,y,yerr)
@@ -59,125 +55,44 @@ print '#####################################'
 
 ###############################################################################
 #TESTS FOR THE OPTIMIZATION
+print 'test 5 - optimization'
+kernel1=kl.Exponential(10.0,1.0)+kl.WhiteNoise(1.0)
+print 'kernel 1 ->', kernel1
+likelihood1=lk.likelihood(kernel1,x,y,yerr)
+print 'likelihood 1 ->', likelihood1
 
-#print '########## Calculations from george ##########'
-##kernel = ge.ExpSine2Kernel(2.0/1.1**2, 7.1)
-#kernel = ge.ExpSine2Kernel(2.0/1.7**2,5.0)
-#gp = george.GP(kernel)
-#gp.compute(x,yerr)
-#
-#print 'initial kernel_george ->', kernel
-#print 'initial likelihood_george ->', gp.lnlikelihood(y)
-#### OPTIMIZE HYPERPARAMETERS
-#import scipy.optimize as op
-## Define the objective function (negative log-likelihood in this case).
-#def nll(p):
-#    # Update the kernel parameters and compute the likelihood.
-#    gp.kernel[:] = p
-#    ll = gp.lnlikelihood(y, quiet=True)
-#
-#    # The scipy optimizer doesn't play well with infinities.
-#    return -ll if np.isfinite(ll) else 1e25
-#
-## And the gradient of the objective function.
-#def grad_nll(p):
-#    # Update the kernel parameters and compute the likelihood.
-#    gp.kernel[:] = p
-#    return -gp.grad_lnlikelihood(y, quiet=True)
-#
-## You need to compute the GP once before starting the optimization.
-#gp.compute(x,yerr)
-#
-#p0 = gp.kernel.vector
-#results = op.minimize(nll, p0, jac=grad_nll)
-#
-## Update the kernel and print the final log-likelihood.
-#gp.kernel[:] = results.x
-#
-#print 'final kernel_george ->', kernel #kernel final
-#print 'final likelihood_george ->', gp.lnlikelihood(y)
-#print '#####################################'
-#
-#
-#print '########## Calculations from gedi ##########'
-#kernel1=kl.ExpSineGeorge(2.0/1.7**2,5.0)
-#print 'initial kernel',kernel2
-#kernel1_result1=lk.likelihood(kernel1,x,y,yerr)
-#
-##optimization
-#print 'EXAMPLE 1a - BFGS'
-#kernel1_resulta=opt.optimization(kernel1,x,y,yerr,method='BFGS')
-#print(kernel1_resulta)
-#
-##optimization
-#print 'EXAMPLE 1b - SDA'
-#kernel1_resultb=opt.optimization(kernel1,x,y,yerr,method='SDA')
-#print(kernel1_resultb)
-#
-##optimization
-#print 'EXAMPLE 1c - RPROP'
-#kernel1_resultc=opt.optimization(kernel1,x,y,yerr,method='RPROP')
-#print(kernel1_resultc)
-#
-##optimization
-#print 'EXAMPLE 1d - altSDA'
-#kernel1_resultc=opt.optimization(kernel1,x,y,yerr,method='altSDA')
-#print(kernel1_resultc)
-#print '#####################################'
-#
-#
-#print '########## Calculations from gedi ##########'
-#kernel2=kl.ExpSineSquared(2.1,1.5,10.2)
-#print 'initial kernel',kernel2
-#kernel2_result1=lk.likelihood(kernel2,x,y,yerr)
-#
-##optimization
-#print 'EXAMPLE 2a - BFGS'
-#kernel2_resulta=opt.optimization(kernel2,x,y,yerr,method='BFGS')
-#print kernel2_resulta
-#
-##optimization
-#print 'EXAMPLE 2b - SDA'
-#kernel2_resultb=opt.optimization(kernel2,x,y,yerr,method='SDA')
-#print kernel2_resultb
-#
-##optimization
-#print 'EXAMPLE 2c - RPROP'
-#kernel2_resultc=opt.optimization(kernel2,x,y,yerr,method='RPROP')
-#print(kernel2_resultc)
-#
-##optimization
-#print 'EXAMPLE 2d - altSDA'
-#kernel2_resultc=opt.optimization(kernel2,x,y,yerr,method='altSDA')
-#print(kernel2_resultc)
-#print '#####################################'
-#
-#
-#print '########## Calculations from gedi ##########'
-#kernel3=kl.ExpSineSquared(1.5,0.6,11.0)
-#print 'initial kernel',kernel3
-#kernel3_result1=lk.likelihood(kernel3,x,y,yerr)
-#
-##optimization
-#print 'EXAMPLE 3a - BFGS'
-#kernel3_resulta=opt.optimization(kernel3,x,y,yerr,method='BFGS')
-#print kernel3_resulta
-#
-##optimization
-#print 'EXAMPLE 3b - SDA'
-#kernel3_resultb=opt.optimization(kernel3,x,y,yerr,method='SDA')
-#print kernel3_resultb
-#
-##optimization
-#print 'EXAMPLE 3c - RPROP'
-#kernel3_resultc=opt.optimization(kernel3,x,y,yerr,method='RPROP')
-#print(kernel3_resultc)
-#
-##optimization
-#print 'EXAMPLE 3c - altSDA'
-#kernel3_resultc=opt.optimization(kernel3,x,y,yerr,method='altSDA')
-#print(kernel3_resultc)
-#print '#####################################'
-#
-#### Conclusion: I don't trust RPROP's logic
-#### I need to check were the None comes from, I have no idea for now...
+optimization1=opt.committed_optimization(kernel1,x,y,yerr,max_opt=2)
+print 'kernel 1 final ->',optimization1[1]
+print 'likelihood 1 final ->', optimization1[0]
+
+###############################################################################
+#TESTS FOR GRAPHICS
+
+kernel2=kl.ExpSineSquared(10.0,1.0,10.0)+kl.Exponential(5.0,1.5)
+xcalc=np.linspace(0,10,200)  
+
+[mu,std]=lk.compute_kernel(kernel1,x,xcalc,y,yerr)
+pl.figure()
+pl.fill_between(xcalc, mu+std, mu-std, color="k", alpha=0.1)
+pl.plot(xcalc, mu+std, color="k", alpha=1, lw=0.25)
+pl.plot(xcalc, mu-std, color="k", alpha=1, lw=0.25)
+pl.plot(xcalc, mu, color="k", alpha=1, lw=0.5)
+pl.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
+pl.title("Pre-optimization")
+pl.xlabel("$x$")
+pl.ylabel("$y$")
+
+optimization1=opt.committed_optimization(kernel2,x,y,yerr,max_opt=10)
+print 'final kernel ->',optimization1[1]
+print 'final likelihood ->', optimization1[0]
+
+[mu,std]=lk.compute_kernel(optimization1[1],x,xcalc,y,yerr)
+pl.figure() 
+pl.fill_between(xcalc, mu+std, mu-std, color="k", alpha=0.1)
+pl.plot(xcalc, mu+std, color="k", alpha=1, lw=0.25)
+pl.plot(xcalc, mu-std, color="k", alpha=1, lw=0.25)
+pl.plot(xcalc, mu, color="k", alpha=1, lw=0.5)
+pl.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
+pl.title('Pos-optimization')
+pl.xlabel("$x$")
+pl.ylabel("$y$")
