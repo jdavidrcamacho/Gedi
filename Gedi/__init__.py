9d97fe2d32e43f2c604d0d06bd5794fb6fadd0a6
jdavidrcamacho <jdavidrcamacho@gmail.com>
2017-03-14 12:05:36 +0000

    necessary scripts to Gedi to work

---
+++ Gedi/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+##### Necessary scripts to everything work #####
+"""
+    Kernel: contains all the developed kernels
+    
+    Kernel_likelihood: contains all the programs for the log likelihood
+calculation and more
+
+    Kernel_optimization: contains all the optimization algorithms implemented
+"""
+import Kernel as kernel
+import Kernel_likelihood as likelihood 
+import Kernel_optimization as optimization
