0c4f5dd1f89ec01cf30b4989acaf1bd91f5c8889
jdavidrcamacho <jdavidrcamacho@gmail.com>
2017-03-14 15:06:49 +0000

    setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+  name = 'Gedi',
+  packages = ['Gedi'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Package to analyze radial velocity measurments using Gaussian processes ',
+  author = 'Joao Camacho',
+  author_email = 'joao.camacho@astro.up.pt',
+  url = 'https://github.com/jdavidrcamacho/Gedi', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['Gaussian', 'process','RV'], # arbitrary keywords
+  classifiers = [],
+)
