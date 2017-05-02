8ff4370d501452db6555a2955b9246c6bf14b186
jdavidrcamacho <jdavidrcamacho@gmail.com>
2017-05-02 21:10:01 +0100

    just tests

--- Tests.py
+++ Tests.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pylab as pl
 
 #####  INITIAL DATA ###########################################################
-np.random.seed(12345)
+#np.random.seed(12345)
 x = 10 * np.sort(np.random.rand(101))
 yerr = 0.2 * np.ones_like(x)
 y = np.sin(x) + yerr * np.random.randn(len(x))
@@ -70,11 +70,11 @@ likelihood5=gedi.kernel_likelihood.likelihood(kernel5,x,y,yerr)
 print 'likelihood 5 ->', likelihood5
 
 #this performs the optimization
-optimization5=gedi.kernel_optimization.committed_optimization(kernel5,x,y,yerr)
-#optimization5=gedi.kernel_optimization.single_optimization(kernel5,x,y,yerr,method="altsda")
+#optimization5=gedi.kernel_optimization.committed_optimization(kernel5,x,y,yerr)
+optimization5=gedi.kernel_optimization.single_optimization(kernel5,x,y,yerr,method="altsda")
 #it returns optimization[0]=final log likelihood optimization[1]=final kernel
-print 'kernel 1 final ->',optimization5[1]
-print 'likelihood 1 final ->', optimization5[0]
+print 'kernel 5 final ->',optimization5[1]
+print 'likelihood 5 final ->', optimization5[0]
 
 print '#####################################'
 
@@ -82,10 +82,13 @@ print '#####################################'
 #TESTS FOR GRAPHICS
 print 'test 6 - everything combined'
 kernel6=gedi.kernel.ExpSineSquared(10.0,1.0,10.0)+gedi.kernel.Exponential(5.0,1.5)
-print 'kernel =',kernel6
+print 'kernel 6 ->',kernel6
+#this calculates the initial log likelihood
+likelihood6=gedi.kernel_likelihood.likelihood(kernel6,x,y,yerr)
+print 'likelihood65 ->', likelihood6
+
 
 xcalc=np.linspace(-1,11,300)  
-
 #computation of the initial mean and standard deviation
 [mu,std]=gedi.kernel_likelihood.compute_kernel(kernel6,x,xcalc,y,yerr)
 pl.figure()
@@ -118,57 +121,96 @@ pl.ylabel("$y$")
 
 print '#####################################'
 
-###############################################################################
-#TESTS FOR MCMC
-print 'test 7 - mcmc'
+print 'test 7 - everything combined'
+kernel7=gedi.kernel.ExpSineSquared(10.0,1.0,10.0)+gedi.kernel.WhiteNoise(1.0)
+print 'kernel 7  ->',kernel7
+#this calculates the initial log likelihood
+likelihood7=gedi.kernel_likelihood.likelihood(kernel7,x,y,yerr)
+print 'likelihood 7 ->', likelihood7
 
-#definition of the initial kernel, the values you put in here are not important
-kernel3=gedi.kernel.ExpSineSquared(10,1,10) + gedi.kernel.WhiteNoise(10.0)
-print 'initial kernel =',kernel3
-kernel3_test1= gedi.kernel_likelihood.likelihood(kernel3,x,y,yerr)
-print 'initia likelihood =',kernel3_test1
-
-#the important is the intervel of the parameters, as it will create the initial
-#guess, in this example we believe the amplitude of the ExpSineSquared is 
-#somewhere between 5 and 15, the lenght scale between 1 and 4, the period
-#between 5 and 10 and the white noise amplitude between 0.1 and 1.
-parameters=[[5.0,15.0],[1.0,4.0],[5.0,15.0],[0.1,1]]
-
-#we set the number of runs we want the algorithm to have
-runs=50000
-burns=runs/2
-#lets run our mcmc
-trial=gedi.kernel_mcmc.MCMC(kernel3,x,y,yerr,parameters,runs,burns)
-
-#now lets make graphics of the results
-xt=np.linspace(burns,runs,runs-burns)
-pl.figure()
-pl.title('log marginal likelihood')
-pl.plot(xt,trial[2],'k-')
-
-f, axarr = pl.subplots(2, 2)
-axarr[0, 0].plot(xt, trial[3][0])
-axarr[0, 0].set_title('amplitude')
-axarr[0, 1].plot(xt, trial[3][1])
-axarr[0, 1].set_title('lenght scale')
-axarr[1, 0].plot(xt, trial[3][2])
-axarr[1, 0].set_title('period')
-axarr[1, 1].plot(xt, trial[3][3])
-axarr[1, 1].set_title('white noise')
-pl.setp([a.get_xticklabels() for a in axarr[0, :]], visible=False)
-
-# computation  of the final kernel
 xcalc=np.linspace(-1,11,300)  
-[mu,std]=gedi.kernel_likelihood.compute_kernel(trial[0],x,xcalc,y,yerr)
-pl.figure() #Graphics
+#computation of the initial mean and standard deviation
+[mu,std]=gedi.kernel_likelihood.compute_kernel(kernel7,x,xcalc,y,yerr)
+pl.figure()
 pl.fill_between(xcalc, mu+std, mu-std, color="k", alpha=0.1)
 pl.plot(xcalc, mu+std, color="k", alpha=1, lw=0.25)
 pl.plot(xcalc, mu-std, color="k", alpha=1, lw=0.25)
 pl.plot(xcalc, mu, color="k", alpha=1, lw=0.5)
 pl.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
-pl.title('After the mcmc')
-pl.xlabel("$time$")
+pl.title("Pre-optimization")
+pl.xlabel("$x$")
 pl.ylabel("$y$")
 
-print 'final kernel =',trial[0]
-print 'final likelihood =',trial[1]
+#run of the optimization algorithms
+optimization7=gedi.kernel_optimization.committed_optimization(kernel7,x,y,yerr,max_opt=10)
+#optimization7=gedi.kernel_optimization.single_optimization(kernel7,x,y,yerr,method="altsda")
+print 'final kernel = ',optimization7[1]
+print 'final likelihood = ', optimization7[0]
+
+#computation of the final mean and standard deviation
+[mu,std]=gedi.kernel_likelihood.compute_kernel(optimization7[1],x,xcalc,y,yerr)
+pl.figure() 
+pl.fill_between(xcalc, mu+std, mu-std, color="k", alpha=0.1)
+pl.plot(xcalc, mu+std, color="k", alpha=1, lw=0.25)
+pl.plot(xcalc, mu-std, color="k", alpha=1, lw=0.25)
+pl.plot(xcalc, mu, color="k", alpha=1, lw=0.5)
+pl.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
+pl.title('Pos-optimization')
+pl.xlabel("$x$")
+pl.ylabel("$y$")
+
+print '#####################################'
+################################################################################
+##TESTS FOR MCMC
+#print 'test 7 - mcmc'
+#
+##definition of the initial kernel, the values you put in here are not important
+#kernel3=gedi.kernel.ExpSineSquared(10,1,10) + gedi.kernel.WhiteNoise(10.0)
+#print 'initial kernel =',kernel3
+#kernel3_test1= gedi.kernel_likelihood.likelihood(kernel3,x,y,yerr)
+#print 'initia likelihood =',kernel3_test1
+#
+##the important is the intervel of the parameters, as it will create the initial
+##guess, in this example we believe the amplitude of the ExpSineSquared is 
+##somewhere between 5 and 15, the lenght scale between 1 and 4, the period
+##between 5 and 10 and the white noise amplitude between 0.1 and 1.
+#parameters=[[5.0,15.0],[1.0,4.0],[5.0,15.0],[0.1,1]]
+#
+##we set the number of runs we want the algorithm to have
+#runs=50000
+#burns=runs/2
+##lets run our mcmc
+#trial=gedi.kernel_mcmc.MCMC(kernel3,x,y,yerr,parameters,runs,burns)
+#
+##now lets make graphics of the results
+#xt=np.linspace(burns,runs,runs-burns)
+#pl.figure()
+#pl.title('log marginal likelihood')
+#pl.plot(xt,trial[2],'k-')
+#
+#f, axarr = pl.subplots(2, 2)
+#axarr[0, 0].plot(xt, trial[3][0])
+#axarr[0, 0].set_title('amplitude')
+#axarr[0, 1].plot(xt, trial[3][1])
+#axarr[0, 1].set_title('lenght scale')
+#axarr[1, 0].plot(xt, trial[3][2])
+#axarr[1, 0].set_title('period')
+#axarr[1, 1].plot(xt, trial[3][3])
+#axarr[1, 1].set_title('white noise')
+#pl.setp([a.get_xticklabels() for a in axarr[0, :]], visible=False)
+#
+## computation  of the final kernel
+#xcalc=np.linspace(-1,11,300)  
+#[mu,std]=gedi.kernel_likelihood.compute_kernel(trial[0],x,xcalc,y,yerr)
+#pl.figure() #Graphics
+#pl.fill_between(xcalc, mu+std, mu-std, color="k", alpha=0.1)
+#pl.plot(xcalc, mu+std, color="k", alpha=1, lw=0.25)
+#pl.plot(xcalc, mu-std, color="k", alpha=1, lw=0.25)
+#pl.plot(xcalc, mu, color="k", alpha=1, lw=0.5)
+#pl.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
+#pl.title('After the mcmc')
+#pl.xlabel("$time$")
+#pl.ylabel("$y$")
+#
+#print 'final kernel =',trial[0]
+#print 'final likelihood =',trial[1]
