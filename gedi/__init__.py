904076f968f911d6a5f4011aff121846847ed663
jdavidrcamacho <jdavidrcamacho@gmail.com>
2018-04-09 09:13:55 +0100

    small change

--- gedi/__init__.py
+++ gedi/__init__.py
@@ -1,5 +1,5 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 ##### Necessary scripts to everything work #####
 """
     Package to analyze radial velocity measurements using Gaussian processes.
